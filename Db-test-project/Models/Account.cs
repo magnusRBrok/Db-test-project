c8c01efac85016f460b8207968b4fd5241eeb333
magnusbrok <90239510+magnusRBrok@users.noreply.github.com>
2024-04-29 21:15:06 +0200

    Add models

---
+++ Db-test-project/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace DB_Test_API.Models;
+
+public class Account
+{
+    public int Id { get; set; }
+    public  double Balance { get; set; }
+    public int CustomerId { get; set; }
+    public List<Transaction>? Transactions { get; set; }
+}
---
+++ Db-test-project/Models/Transaction.cs
@@ -0,0 +1,10 @@
+﻿namespace DB_Test_API.Models;
+
+public class Transaction
+{
+    public int Id { get; set; }
+    public float Amount { get; set; }
+    public int RecipientAccountId { get; set; }
+    public int SenderAccountId {  get; set; }
+    public DateTime Timestamp { get; set; }
+}
