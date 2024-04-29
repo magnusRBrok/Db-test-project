4c164b5168cd9dd3ddc64d9658122c2954da17c8
magnusbrok <90239510+magnusRBrok@users.noreply.github.com>
2024-04-29 21:16:53 +0200

    Create AccountController.cs

---
+++ Db-test-project/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using DB_Test_API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DB_Test_API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AccountController : ControllerBase
+{
+
+    private readonly ILogger<AccountController> _logger;
+    private readonly IAccountService _accountService;
+
+    public AccountController(ILogger<AccountController> logger, IAccountService accountService)
+    {
+        _logger = logger;
+        _accountService = accountService;
+    }
+
+    [HttpGet(Name = "GetAccountBalance")]
+    public double GetAccountBalance(int id)
+    {
+        return 0;
+    }
+}
