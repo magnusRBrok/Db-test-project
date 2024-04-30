db54b7b1d2033f1a52feacc347f4a7924a3d72f0
magnusbrok <90239510+magnusRBrok@users.noreply.github.com>
2024-04-30 22:46:29 +0200

    Add PaymentController (not implemented)

---
+++ Db-test-project/Controllers/PaymentController.cs
@@ -0,0 +1,37 @@
+﻿using DB_Test_API.Models;
+using Db_test_project.DTOs.Requests.Create;
+using Db_test_project.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Db_test_project.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PaymentController : Controller
+    {
+        private readonly IPaymentService _PaymentService;
+
+        public PaymentController(IPaymentService paymentService)
+        {
+            _PaymentService = paymentService;
+        }
+
+        [HttpPost("Deposit")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Transaction> Deposit(PaymentDTO paymentDTO)
+        {
+            return null;
+        }
+
+        [HttpPost("Withdraw")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Transaction> Withdraw(PaymentDTO paymentDTO)
+        {
+            return null;
+        }
+    }
+}
---
+++ Db-test-project/DTOs/Requests/Create/PaymentDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Db_test_project.DTOs.Requests.Create
+{
+    public class PaymentDTO
+    {
+        public int AccountId { get; set; }
+        public double Amount { get; set; }
+    }
+}
